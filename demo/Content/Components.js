378b03e9169e7d701ccd4aca327adcb2939e70b2
zhuang.miao <zhuang.miao@tongdun.net>
2019-08-28 17:08:09 +0800

    push

--- demo/Content/Components.js
+++ demo/Content/Components.js
@@ -7,7 +7,7 @@ export default [
 			{
 				component: "read",
 				type: "iconNodeInput",
-				iconPath: "/jobTypes/数据引入@2x.png",
+				iconPath: "./jobTypes/数据引入@2x.png",
 				name: "输入",
 				help: `支持对节点的形状大小完全自定义`
 			}
@@ -21,7 +21,7 @@ export default [
 			{
 				component: "write",
 				type: "iconNodeOutput",
-				iconPath: "/jobTypes/数据存储@2x.png",
+				iconPath: "./jobTypes/数据存储@2x.png",
 				name: "输出",
 				help: `基于Snap.svg可以方便的进行动画操作`
 			}
@@ -35,7 +35,7 @@ export default [
 			{
 				component: "select",
 				type: "iconNode",
-				iconPath: "/jobTypes/数据转换@2x.png",
+				iconPath: "./jobTypes/数据转换@2x.png",
 				name: "过程组件",
 				help: `这是一个使用MMEditor的基础例子`
 			}
